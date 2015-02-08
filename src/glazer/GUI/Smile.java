6c50ae9c0a40c1ecbc3a1a545c75ae2f29781fb5
shifraglazer <shifraglazer@gmail.com>
2015-02-08 15:51:14 -0500

    Smile wink

--- src/glazer/GUI/Smile.java
+++ src/glazer/GUI/Smile.java
@@ -2,6 +2,9 @@ package glazer.GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 
@@ -11,28 +14,40 @@ public class Smile extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Container container;
+	private ScheduledExecutorService executor;
 	public Smile(){
 		setSize(800,800);
 		setTitle("Smile");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container container= getContentPane();
-		
+		container= getContentPane();
+		executor = Executors.newScheduledThreadPool(1);
+		SmileComponent smile=new SmileComponent(container);
+		Runnable paint = new Runnable() {
+			public void run() {
+				smile.setSize(container.getSize());
+			container.repaint();
+			}
+		};
+		executor.scheduleAtFixedRate(paint, 0, 10, TimeUnit.MILLISECONDS);	
 		container.setLayout(new BorderLayout());
-		SmileComponent smile=new SmileComponent();
 		
-		container.add(smile);
-		smile.setSize(container.getSize());
+		
+		container.add(smile,BorderLayout.CENTER);
+		
 	}
 	 public static void main(String args[]){
 		 Smile s=new Smile();
 		 s.setVisible(true);
+		 /*
 		 Thread t=new Thread(){
 		
 			 public void run(){
 				 while(true){
+					
 				 s.repaint();
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(10);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -41,6 +56,6 @@ public class Smile extends JFrame{
 			 } 
 	 };
 		 t.start();
-
+*/
 }
 }
--- src/glazer/GUI/SmileComponent.java
+++ src/glazer/GUI/SmileComponent.java
@@ -1,12 +1,16 @@
 package glazer.GUI;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Graphics;
 
 import javax.swing.JComponent;
 
-public class SmileComponent extends JComponent{
+public class SmileComponent extends JComponent {
 	private int count;
+	private boolean close;
+	private Container container;
+	
 	/**
 	 * 
 	 */
@@ -16,31 +20,112 @@ public class SmileComponent extends JComponent{
 	 * 
 	 */
 
-
 	@Override
 	protected void paintComponent(Graphics g) {
-		count++;
 		super.paintComponent(g);
+	
 		g.setColor(Color.YELLOW);
 		g.fillOval(200, 100, 400, 400);
-		if(count%2==0){
-		
 		g.setColor(Color.BLUE);
-		g.drawArc(325, 200, 25, 25, 0, -180);
-		g.drawArc(450, 200, 25, 25, 0, -180);
+		g.fillOval(325, 200, 25, 25);
+		
+		switch (count) {
+		case 0: {
+			g.fillOval(450, 200, 25, 25);
+			break;
+		}
+		case 1: {
+			g.fillArc(450, 201, 25, 25, 0, 180);
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			break;
+		}
+		case 2: {
+			g.fillArc(450, 203, 25, 25, 0, 180);
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			break;
+		}
+		case 3: {
+			g.fillArc(450, 205, 25, 25, 0, 180);
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			break;
+		}
+		case 4: {
+			g.fillArc(450, 206, 25, 25, 0, 180);
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			break;
+		}
+		case 5: {
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.setColor(Color.YELLOW);
+			g.fillArc(450, 200, 25, 15, 0, -180);
+			break;
+		}
+		case 6: {
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.setColor(Color.YELLOW);
+			g.fillArc(450, 200, 25, 17, 0, -180);
+			break;
+		}
+		case 7: {
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.setColor(Color.YELLOW);
+			g.fillArc(450, 200, 25, 20, 0, -180);
+			break;
+		}
+		case 8: {
+			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.setColor(Color.YELLOW);
+			g.fillArc(450, 200, 25, 22, 0, -180);
+			break;
+		}
+		case 9: {
+			
+			g.drawArc(450, 200, 25, 25, 0, -180);
+			break;
+		}
+		}
+		if(close && count <9){
+			count++;
+		}
+		else if( count==9){
+			close=false;
+			count--;
+			
+		}
+		else if(!close&& count>0){
+		if(count==8){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+			count --;
+		}
+		else if(!close && count==0){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			count++;
+			close=true;
+		}
+		
 	
-		}
-		else{
-			g.setColor(Color.BLUE);
-			g.fillOval(325, 200, 25, 25);
-			g.fillOval(450, 200, 25, 25);	
-		}
+		 
 		g.setColor(Color.RED);
 		g.drawArc(340, 300, 130, 112, 0, -180);
-		
-		
+
 	}
 
-	
-	
+	public SmileComponent(Container container) {
+		this.container=container;
+		this.count = 0;
+		this.close = false;
+		this.setSize(container.getSize());
+	}
+
 }
