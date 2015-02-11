7d16197de9a755e238e84dab655af8e34000dd43
shifraglazer <shifraglazer@gmail.com>
2015-02-11 18:32:25 -0500

    Smile resize

--- src/glazer/GUI/SmileComponent.java
+++ src/glazer/GUI/SmileComponent.java
@@ -23,69 +23,78 @@ public class SmileComponent extends JComponent {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-	
+		int width=container.getWidth();
+		int height=container.getHeight();
+		int smileWidth=(int)(width*.8);
+		int smileHeight=(int)(height*.8);
+		int smileX=(width-smileWidth)/2;
+		int smileY=(height-smileHeight)/2;
 		g.setColor(Color.YELLOW);
-		g.fillOval(200, 100, 400, 400);
+		g.fillOval(smileX,smileY, smileWidth, smileHeight);
 		g.setColor(Color.green);
-		g.fillOval(390, 270, 40,30);
+		g.fillOval((int)((smileWidth/2)+smileX), (int)((smileHeight/2)+smileY), (int)(smileWidth*.10),(int)(smileHeight*.10));
 		g.setColor(Color.BLUE);
-		g.fillOval(325, 200, 25, 25);
-		
+		int eyeWidth=(int)(smileWidth*.08);
+		int eyeHeight=(int)(smileHeight*.08);
+		int eyeY=(int)((smileHeight*.25)+smileY);
+		g.fillOval((int)(smileWidth*.30)+smileX, eyeY, eyeWidth, eyeHeight);
+		int eyeX=(int)(smileWidth*.70)+smileX;
+		int eye2Y=(int)((smileHeight*.25)+smileY);
 		
 		switch (count) {
 		case 0: {
-			g.fillOval(450, 200, 25, 25);
+			g.fillOval(eyeX, eye2Y, eyeWidth, eyeHeight);
 			break;
 			
 		}
 		case 1: {
-			g.fillArc(450, 201, 25, 25, 0, 180);
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y+1, eyeWidth, eyeHeight, 0, 180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			break;
 		}
 		case 2: {
-			g.fillArc(450, 203, 25, 25, 0, 180);
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y+3, eyeWidth, eyeHeight, 0,180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			break;
 		}
 		case 3: {
-			g.fillArc(450, 205, 25, 25, 0, 180);
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y+5, eyeWidth, eyeHeight, 0, 180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			break;
 		}
 		case 4: {
-			g.fillArc(450, 206, 25, 25, 0, 180);
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y+6, eyeWidth, eyeHeight, 0, 180);
+			g.fillArc(eyeX,eye2Y, eyeWidth, eyeHeight, 0, -180);
 			break;
 		}
 		case 5: {
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			g.setColor(Color.YELLOW);
-			g.fillArc(450, 200, 25, 15, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, (int)(eyeHeight*.6), 0, -180);
 			break;
 		}
 		case 6: {
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			g.setColor(Color.YELLOW);
-			g.fillArc(450, 200, 25, 17, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, (int)(eyeHeight*.68), 0, -180);
 			break;
 		}
 		case 7: {
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			g.setColor(Color.YELLOW);
-			g.fillArc(450, 200, 25, 20, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, (int)(eyeHeight*.9), 0, -180);
 			break;
 		}
 		case 8: {
-			g.fillArc(450, 200, 25, 25, 0, -180);
+			g.fillArc(eyeX, eye2Y,eyeWidth, eyeHeight, 0, -180);
 			g.setColor(Color.YELLOW);
-			g.fillArc(450, 200, 25, 22, 0, -180);
+			g.fillArc(eyeX, eye2Y, eyeWidth, (int)(eyeHeight*.88), 0, -180);
 
 			break;
 		}
 		case 9: {
 			
-			g.drawArc(450, 200, 25, 25, 0, -180);
+			g.drawArc(eyeX, eye2Y, eyeWidth, eyeHeight, 0, -180);
 			break;
 		}
 		}
@@ -122,7 +131,7 @@ public class SmileComponent extends JComponent {
 	
 		 
 		g.setColor(Color.RED);
-		g.drawArc(340, 300, 130, 112, 0, -180);
+		g.drawArc((int)((smileWidth*.25)+smileX), (int)((smileHeight*.45)+smileY),(int)(smileWidth*.50) , (int)(height*.3), 0, -180);
 
 	}
 
