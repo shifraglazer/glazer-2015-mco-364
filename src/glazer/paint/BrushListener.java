2a4d9c4fa705b6e6afdba30cb5e9caf0bec9e94a
shifraglazer <shifraglazer@gmail.com>
2015-05-26 14:30:18 -0400

    brushListener

--- src/glazer/paint/BrushListener.java
+++ src/glazer/paint/BrushListener.java
@@ -1,34 +1,26 @@
 package glazer.paint;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
 
-public abstract class BrushListener implements MouseListener,MouseMotionListener{
+public abstract class BrushListener implements MouseListener,
+		MouseMotionListener {
 
 	protected Canvas canvas;
 	protected int startX;
-	protected int startY;	
+	protected int startY;
 	protected int lastX;
 	protected int lastY;
-	
-	
+
 	public BrushListener(Canvas canvas) {
 		this.canvas = canvas;
-		BufferedImage image=canvas.getImage();
-		//Graphics graphics=canvas.getImage().getGraphics();
-		//graphics.setColor(Color.WHITE);
-		//graphics.fillRect(0, 0,600 ,600);
-		//graphics.setColor(Color.BLACK);
 	}
-	
-	public Graphics2D setUpGraphics(Graphics g){
+
+	public Graphics2D setUpGraphics(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(canvas.getColor());
 		g2.setStroke(new BasicStroke(canvas.getStroke()));
@@ -37,52 +29,59 @@ public abstract class BrushListener implements MouseListener,MouseMotionListener
 
 	@Override
 	public void mouseDragged(MouseEvent event) {
-		lastX=event.getX();
-		lastY=event.getY();
+		lastX = event.getX();
+		lastY = event.getY();
+		canvas.repaint();
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent event) {
 		// TODO Auto-generated method stub
-		startX=event.getX();
-		startY=event.getY();
-		
+		startX = event.getX();
+		startY = event.getY();
+		lastX = event.getX();
+		lastY = event.getY();
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent event) {
-		lastX=event.getX();
-		lastY=event.getY();
-		draw(canvas.getImage().getGraphics());	
+		lastX = event.getX();
+		lastY = event.getY();
+		draw(canvas.getImage().getGraphics());
 		canvas.commitHistory(canvas.getImage());
 		canvas.repaint();
+		startX = lastX;
+		startY = lastY;
 	}
-	 abstract public void draw(Graphics g);
-	
 
-	
+	abstract public void draw(Graphics g);
+
+	abstract void drawIcon(Graphics g);
+
 }
